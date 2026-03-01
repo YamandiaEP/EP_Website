bcd742393648ad92a0c10c4128620c5e645f6255
mateusz-rudzinski <rudzinski.eg@gmail.com>
2026-03-01 20:32:28 +0100

    fix: base URL slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://YamandiaEP.github.io',
-  base: '/EP_Website',
+  site: 'https://yamandiaep.github.io',
+  base: '/EP_Website/',
 });
